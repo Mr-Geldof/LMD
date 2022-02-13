4700df1cc29052f004fe147efb7bf6dd65d7cf95
Mr-Geldof <94635185+Mr-Geldof@users.noreply.github.com>
2022-02-13 00:00:21 +0000

    Add files via upload

---
+++ LMD/index.sql
@@ -0,0 +1,7 @@
+INSERT INTO VALUES (P01,Samsung S20, Smartphone, 3299),(P02, ASUS NoteBook, PC, 4599);
+
+INSERT INTO Customer VALUES (C01,Ali,7132009),(C02,Asma,77345823);
+
+INSERT INTO Orders (Customer_id, Porduct_id, Quantity, Total_amount) VALUES (C01,P02,2,9197);
+
+INSERT INTO Orders VALUES(C02,P01,28/05/2020,1,3299);
